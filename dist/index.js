c612ceeba23610d4c311f00b5190a165a5f9c8d0
Ajay Wankhade <ajaysw45@gmail.com>
2024-11-20 21:26:09 +0530

    types, interfaces , alias

--- dist/index.js
+++ dist/index.js
@@ -6,4 +6,110 @@ function add(a, b) {
 }
 const y = add(1, 2);
 console.log(y);
+let num = 1234234324;
+let num1 = 2342344352342454324543352n;
+console.log(num);
+console.log(num1);
+let text = "My name is ajay wankhade";
+let isAvailable = true;
+let nums = [1, 5, 23, 435, 2];
+let charArray = ['as', 'asdb', 'adfa'];
+let val = null;
+let user;
+console.log(user);
+let obj = {
+    name: 'Ajay',
+    age: 20
+};
+obj = { ...obj, age: 44, name: "Wankhade" };
+console.log(obj);
+let value;
+value = 10;
+value = 'a';
+function testAnyType(txt) {
+    console.log(txt);
+}
+let numArray = [23, 34, 23, 2, 2, 23];
+let chArray = ['a', 's'];
+let tup = [1, 'a'];
+let coordinates = [10, 20, 30];
+let reponse = [201, "Success"];
+var Color;
+(function (Color) {
+    Color["RED"] = "red";
+    Color["GREEN"] = "green";
+    Color["BLUE"] = "blue";
+})(Color || (Color = {}));
+console.log(Color.BLUE);
+let size = 2;
+console.log(size);
+let notSure = 'a';
+if (typeof (notSure) === 'number') {
+    notSure.toFixed(2);
+}
+else if (typeof (notSure) === 'string') {
+    notSure.length;
+}
+function infiniteLoop() {
+    while (true) {
+        console.log(1);
+    }
+}
+function throwError(message) {
+    throw new Error(message);
+}
+function logMessage(message) {
+    console.log(message);
+}
+logMessage("Lets Nacho");
+let username = 'Nikhil';
+let someVal = 'Ajju Bhai ';
+let strLength = someVal.length;
+console.log(strLength);
+let id;
+id = 'abc';
+id = 123;
+function printId(id) {
+    console.log('id : ${ id }');
+}
+printId('xyz');
+printId('4543');
+function printIdfn(id) {
+    if (typeof (id) === 'string') {
+        console.log('id is a string : ', id.toUpperCase);
+    }
+    else {
+        console.log('id is a number : ${ id } ');
+    }
+}
+printIdfn('ajjub bhai ');
+let person = {
+    name: 'ajay',
+    age: 24,
+    greet: function () {
+        console.log("Function not implemented.");
+    }
+};
+person.greet();
+const add1 = (x, y) => x + y;
+const subtract = (x, y) => x - y;
+console.log(subtract(5, 4));
+console.log(add1(5, 4));
+let userId = 'abc';
+let person1 = {
+    name: 'abc',
+    age: 12
+};
+let userId1 = 'abc';
+let orderId = 101;
+let myCustomer = {
+    address: "ngp",
+    name: "xyz",
+    phoen: 1231431
+};
+let myCar = {
+    name: 'maruti',
+    modelNo: 800,
+    isElectric: true
+};
 //# sourceMappingURL=index.js.map
--- ts/index.ts
+++ ts/index.ts
@@ -64,7 +64,206 @@ let coordinates:[number, number,number]=[10,20,30];
 //http reponse
 let reponse: [number, string ]= [201,"Success"];
 
+//enum 
+enum Color {
+    RED= 'red', 
+    GREEN = 'green',
+    BLUE ='blue'
+}
+
+console.log(Color.BLUE);
+
+// enum Size {
+//     Small=1,
+//     Medium,
+//     Large
+// }
+
+const enum Size {
+    Small=1,
+    Medium,
+    Large
+}
+
+
+let size : Size = Size.Medium;
+console.log(size);
+
+
+//unknown type 
+let notSure: unknown = 'a';
+
+if(typeof(notSure) === 'number'){
+    notSure.toFixed(2);
+}else if(typeof(notSure) === 'string'){
+    notSure.length;
+}
+
+
+//never : types which never occured
+
+function infiniteLoop(): never{
+    while(true){
+        //do something endlessly
+        console.log(1);
+        
+    }
+}
+
+// infiniteLoop();
+
+
+function throwError(message: string ) : never{
+    throw new Error(message);
+}
+
+// throwError("Something went wrong")
+
+//void type 
+
+function logMessage(message:string ):void{
+    console.log(message);
+    // return undefined;
+    
+}
+
+logMessage("Lets Nacho");
 
 
 
+//type inference 
 
+let username = 'Nikhil'; //typescript infers the type as string 
+
+// type assertions 
+let someVal : any = 'Ajju Bhai ';
+let strLength : number = (someVal as string).length;
+
+console.log(strLength);
+
+
+// union types 
+let id : string | number; // id can be strign or number
+
+id = 'abc';
+id = 123;
+
+
+function printId(id: string | number){
+    console.log('id : ${ id }');
+    
+}
+
+printId('xyz');
+printId('4543');
+
+//type narrowing 
+
+function printIdfn(id: string | number){
+    if(typeof(id)=== 'string'){
+        console.log('id is a string : ', id.toUpperCase);
+        
+    }else{
+        console.log('id is a number : ${ id } ');
+        
+    }
+    
+}
+
+printIdfn('ajjub bhai ')
+
+
+//interface 
+
+interface Person {
+    name : string ;
+    age : number;
+    greet() : void;
+}
+
+
+let person : Person = {
+    name: 'ajay',
+    age: 24,
+    greet: function (): void {
+        console.log("Function not implemented.");
+    }
+}
+
+
+person.greet();
+
+
+
+//interface with function types
+interface MathOp{
+    (a:number, b:number) : number;
+}
+
+const add1 : MathOp = (x,y) => x+y;
+const subtract : MathOp = (x,y) => x-y;
+
+
+console.log(subtract(5,4));
+console.log(add1(5,4));
+
+
+//type alias
+
+//type AliasName = TypeDefinition : to give alias ot the types 
+
+type UserId = string; 
+let userId: UserId = 'abc';
+
+
+type Person1 = {
+    name: string;
+    age: number;
+};
+let person1: Person1 = {
+    name: 'abc',
+    age: 12
+};
+
+
+type ID = string | number ; 
+
+let userId1 : ID = 'abc';
+
+let orderId : ID = 101;
+
+//interfaces vs type
+
+interface User {
+    name : string ;
+    phoen : number
+}
+
+interface Customer extends User{
+    address : string 
+}
+
+
+let myCustomer : Customer = {
+    address: "ngp",
+    name: "xyz",
+    phoen: 1231431
+}
+
+type Vehicle = {
+    name : string,
+    modelNo : number 
+}
+
+
+// & - denotes intersetion types 
+type Car = Vehicle & {
+    isElectric : boolean;
+}
+
+
+let myCar: Car ={
+    name : 'maruti',
+    modelNo : 800,
+    isElectric : true
+}
